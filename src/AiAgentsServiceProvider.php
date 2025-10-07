919018297e04a241696577d69fb613d264dddd7b
Muhammad Junaid Rehman Siddiqui <jbhai8058@gmail.com>
2025-10-07 18:52:03 +0500

    fix: update namespace and directory structure

--- src/AiAgentsServiceProvider.php
+++ src/AiAgentsServiceProvider.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace MuhammadJunaidRehmanSiddiqui\AiAgents;
+namespace MuhammadJunaidRehmanSiddiqui\LaravelAiAgents;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Artisan;
-use MuhammadJunaidRehmanSiddiqui\AiAgents\AiAgentManager;
+use MuhammadJunaidRehmanSiddiqui\LaravelAiAgents\AiAgentManager;
 
 class AiAgentsServiceProvider extends ServiceProvider
 {
